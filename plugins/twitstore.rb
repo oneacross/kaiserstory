d16b3149a80543afb55fe0ee83809a384378011f
Matt Mendell <matthew.mendell@gmail.com>
2011-04-16 17:16:46 -0700

    greatly improve the ruby script
    actually import stuff into database!

--- plugins/twitstore.rb
+++ plugins/twitstore.rb
@@ -1,5 +1,32 @@
 #!/usr/bin/env ruby
 
 require 'mysql'
+require 'json'
 
-conn = Mysql.new('localhost', 'mmendell', 'kaiser')
+conn = Mysql.new('localhost', 'mmendell', 'kaiser', 'kaiser')
+
+# convert the json text into a table text for mysql
+filename = 'twitraw/Apr_16_2011_12_29_48.txt'
+kw = "obama"
+
+File.open(filename).each { |line|
+    begin
+        tweet = JSON.load(line)
+    rescue JSON::ParserError
+        # ignore lines that are not legel JSON
+        next
+    end
+
+    media = 'twitter'
+    text = conn.quote(tweet['text'])
+    location = conn.quote(tweet['location'])
+
+    # newssource newsdate content location politician_name
+    conn.query("insert into medias (newssource, newsdate, content, location, politician_name) " +
+               "values (\"#{media}\", \"April 2011\", \"#{text}\", \"#{location}\", \"#{kw}\")")
+}
+
+# TODO, loading a file will be faster
+#stmt = "LOAD DATA LOCAL INFILE \"\" INTO TABLE kaiser"
+
+conn.close()
